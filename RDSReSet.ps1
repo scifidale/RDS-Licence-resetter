6b115f45713f3a8869463be8eb0ca97c2784994b
scifidale-Desktop <49734424+scifidale@users.noreply.github.com>
2020-05-11 16:02:51 +0100

    updated script
    finds terminal server grace licence key, resets the permissions and deletes the key.
    XML file is example scheduled task to import into environment

--- RDSReSet.ps1
+++ RDSReSet.ps1
@@ -1,10 +1,15 @@
 # RDS Lab Licence Resetter 
 
-$User = Everyone
-$RDSServer = Test-path -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
-if ($RDSServer = "True") {.\SetACL.exe -on "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" -ot reg -actn setowner -ownr "n:$User"
-SetACL.exe -on "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" -ot reg -actn ace -ace "n:$User"
-Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod\L$RT*"
-eventcreate /id 555 /D "This Lab RDS Server has sucessfully reset its grace period" /T Information /L Application /so RDSLAB}
 
+$User = "Everyone"
+$RDSLicKey = 'HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
+$RDSServer = Test-path -path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod"
+if ($RDSServer -eq "True") {.\Set-ACL\SetACL.exe -on "$RDSLicKey" -ot reg -actn setowner -ownr "n:$User"
+.\Set-ACL\SetACL.exe -on "$RDSLicKey" -ot reg -actn ace -ace "n:$User;p:full"
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" -Recurse
+eventcreate /id 555 /D "This Lab RDS Server has sucessfully reset its grace period" /T Information /L Application /so RDSLAB
 Exit
+} ELSE {
+Eventcreate /id 555 /D "This is not an RDS server grace period will not be reset" /T Information /L Application /so RDSLAB
+Exit
+}
